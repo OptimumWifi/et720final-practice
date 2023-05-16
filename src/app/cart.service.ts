2c79427d6e6223445d310837c0043b50b39e7d95
OptimumWifi <123668497+OptimumWifi@users.noreply.github.com>
2023-05-15 21:42:40 -0400

    2 files modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -8,5 +8,19 @@ export class CartService {
   
   items: Product[] = [];
 
+  addToCart(product: Product) {
+    this.items.push(product);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+
+
   constructor() { }
 }
